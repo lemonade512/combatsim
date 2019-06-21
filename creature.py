866fdabc718ca759320d9c0d07f46617e603bba1
Phillip Lemons <philliplemons512@gmail.com>
2019-06-20 20:35:17 -0500

    Implement extremely simple and naive simulator

---
+++ creature.py
@@ -0,0 +1,83 @@
+""" Defines a creature that can be used in the simulator. """
+
+import math
+
+from dice import Dice
+
+# Attributes:
+#   Name, level, xp, proficiency, abilities, HD, HP,
+#   AC, initiative, attacks
+
+# Ignoring for now:
+#   spellcasting
+
+
+# TODO (phillip): Figure out a good way to specify base creature
+class Creature:
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name', "nameless")
+        self.level = kwargs.get('level', 0)
+        self.xp = kwargs.get('xp', None)
+        self.proficiency = kwargs.get(
+            'proficiency', 1 + math.ceil(self.level / 4)
+        )
+        self.hp = kwargs.get('hp', 10)
+        self._strength = kwargs.get('strength', 10)
+        self._dexterity = kwargs.get('dexterity', 10)
+        self._constitution = kwargs.get('constitution', 10)
+        self._intelligence = kwargs.get('intelligence', 10)
+        self._wisdom = kwargs.get('wisdom', 10)
+        self._charisma = kwargs.get('charisma', 10)
+
+        self.str = self._ability_bonus(self._strength)
+        self.dex = self._ability_bonus(self._dexterity)
+        self.con = self._ability_bonus(self._constitution)
+        self.int = self._ability_bonus(self._intelligence)
+        self.wis = self._ability_bonus(self._wisdom)
+        self.cha = self._ability_bonus(self._charisma)
+
+    def __str__(self):
+        return f"{self.name}"
+
+    __repr__ = __str__
+
+    def attack(self, other):
+        print(f"{self} attacks {other}")
+        other.hp -= 5
+
+    def choose_target(self, others):
+        for creature in others:
+            if creature.hp > 0:
+                return creature
+
+        return None
+
+    #def act(self, enemies, allies):
+    def act(self, others):
+        if not self.is_alive():
+            return
+        target = self.choose_target(others)
+        self.attack(target)
+
+    def is_alive(self):
+        return self.hp > 0
+
+    @staticmethod
+    def _ability_bonus(ability):
+        return math.floor(int(ability) / 2 - 5)
+
+    @property
+    def stat_block(self):
+        out = f"Creature({self.name})\n"
+        out += f"\tStr: {self.strength} ({self._ability_bonus(self.strength)})"
+        out += f"  Dex: {self.dexterity} ({self._ability_bonus(self.dexterity)})"
+        out += f"  Con: {self.constitution} ({self._ability_bonus(self.constitution)})"
+        out += f"\n\tInt: {self.intelligence} ({self._ability_bonus(self.intelligence)})"
+        out += f"  Wis: {self.wisdom} ({self._ability_bonus(self.wisdom)})"
+        out += f"  Cha: {self.charisma} ({self._ability_bonus(self.charisma)})"
+        return out
+
+
+if __name__ == "__main__":
+    c = Creature()
+    print(c.stat_block)
---
+++ encounter.py
@@ -0,0 +1,52 @@
+from dice import Dice
+
+class Encounter:
+
+    def __init__(self, creatures):
+        self.creatures = creatures
+
+    def run(self):
+        initiative = self.roll_initiative()
+        i = 0
+        while True:
+            current_creature = initiative[i][1]
+            current_creature.act([c for c in self.creatures if c != current_creature])
+            if self.encounter_over():
+                break
+            i = (i + 1) % len(initiative)
+
+        for creature in self.creatures:
+            print(f"{creature}: {creature.hp}")
+
+    def encounter_over(self):
+        alive = 0
+        for creature in self.creatures:
+            if creature.hp > 0:
+                alive += 1
+
+        return alive <= 1
+
+    def roll_initiative(self):
+        """ Rolls initiative for all creatures in the encounter.
+
+        Returns:
+            list: All creatures sorted in initiative order where the creature
+            with the highest initiative roll is at index 0.
+        """
+        initiative = []
+        for creature in self.creatures:
+            roll = Dice.roll('1d20') + creature.dex
+            initiative.append((roll, creature))
+
+        initiative.sort(key=lambda x: x[0], reverse=True)
+        return initiative
+
+
+if __name__ == "__main__":
+    from creature import Creature
+    e = Encounter([
+        Creature(name="Fast Man", dexterity=25),
+        Creature(name="Commoner 1"),
+        Creature(name="Commoner 2")
+    ])
+    e.run()
