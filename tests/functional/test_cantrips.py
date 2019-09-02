d94485103b2f2950bc870721b6170d0f2b33d924
Phillip Lemons <philliplemons512@gmail.com>
2019-09-02 16:21:04 -0500

    Separate unit and functional tests

--- tests/functional/test_cantrips.py
+++ tests/functional/test_cantrips.py
@@ -8,12 +8,10 @@ from combatsim.creature import Creature
 
 from combatsim.tests.mock_dice import MockDice
 
-mock_dice = MockDice()
-
 
 class TestAcidSplash(unittest.TestCase):
 
-    @mock_dice.schedule_cleanup
+    @MockDice.patch('combatsim.creature', 'combatsim.cantrips')
     def test_acid_splash_against_single_enemy(self):
         # Let's say we have a wizard fighting against a kobold. The wizard only
         # knows acid splash, and the two combatants are 60 feet from each other.
@@ -21,13 +19,15 @@ class TestAcidSplash(unittest.TestCase):
         wizard = Creature(level=1, max_hp=6)
         kobold = Creature(level=1)
 
-        # Setup mock dice rolls
-        mock_dice.patch('combatsim.creature', 'combatsim.cantrips')
-        mock_dice.add(kobold.saving_throw, '1d20', mock_dice < wizard.spell_dc)
-        mock_dice.add(cantrips.CantripDamage.activate, '1d8', mock_dice == 1)
+        # The wizard casts acid splash at the kobold, and the kobold passes its
+        # dexterity saving throw, thus receiving no damage.
+        MockDice.set_roll(kobold.saving_throw, '1d20', MockDice.value > wizard.spell_dc)
+        wizard.cast(cantrips.acid_splash, 0, targets=[kobold])
+        self.assertEqual(kobold.hp, 6)
 
         # The wizard casts acid splash at the kobold, and the kobold fails its
         # dexterity save, thus receiving acid damage.
+        MockDice.set_roll(kobold.saving_throw, '1d20', MockDice.value < wizard.spell_dc)
+        MockDice.set_roll(cantrips.CantripDamage.activate, '1d8', MockDice.value == 1)
         wizard.cast(cantrips.acid_splash, 0, targets=[kobold])
-        # TODO: Assert that action was used up
         self.assertEqual(kobold.hp, 6-(1 + wizard.spellcasting))
