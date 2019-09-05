997015f68e71308dfab4a9a6dd5fff77f4944247
Phillip Lemons <philliplemons512@gmail.com>
2019-09-04 21:02:43 -0500

    Improve skeleton for testing cantrips

--- tests/functional/test_cantrips.py
+++ tests/functional/test_cantrips.py
@@ -5,6 +5,7 @@ import unittest
 import combatsim.cantrips as cantrips
 from combatsim.grid import Grid
 from combatsim.creature import Monster
+from combatsim.rules_error import RulesError
 
 from combatsim.tests.mock_dice import MockDice
 
@@ -15,7 +16,7 @@ class TestAcidSplash(unittest.TestCase):
     def test_acid_splash_against_single_enemy(self):
         # Let's say we have a wizard fighting against a kobold. The wizard only
         # knows acid splash, and the two combatants are 60 feet from each other.
-        grid = Grid(1,60)
+        grid = Grid(1,80)
         wizard = Monster(level=1, pos=(0,0), grid=grid)
         kobold = Monster(level=1, max_hp=6, pos=(0,59), grid=grid)
 
@@ -31,3 +32,49 @@ class TestAcidSplash(unittest.TestCase):
         MockDice.set_roll(cantrips.CantripDamage.activate, '1d8', MockDice.value == 1)
         wizard.cast(cantrips.acid_splash, 0, targets=[kobold])
         self.assertEqual(kobold.hp, 6-(1 + wizard.spellcasting))
+
+        # The wizard tries to cast acid splash on a location outside the range
+        # of the spell, resulting in a rules error.
+        self.assertRaises(RulesError, wizard.cast, cantrip.acid_splash, 0, targets=[(0, 70)])
+
+    @MockDice.patch('combatsim.creature', 'combatsim.cantrips')
+    def test_acid_splash_against_two_enemies(self):
+        # Let's say we have a wizard fighting against two kobolds. The wizard
+        # only knows acid splash, and the two kobolds are 60 feet from the
+        # wizard and next to each other.
+        grid = Grid(1,60)
+        wizard = Monster(level=1, pos=(0,0), grid=grid)
+        kobold1 = Monster(level=1, max_hp=6, pos=(0,59), grid=grid)
+        kobold2 = Monster(level=1, max_hp=6, pos=(0,58), grid=grid)
+
+        # The wizard casts acid splash at the kobolds, and one kobold passes
+        # its saving throw while the other fails.
+        MockDice.set_roll(kobold1.saving_throw, '1d20', MockDice.value > wizard.spell_dc)
+        MockDice.set_roll(kobold2.saving_throw, '1d20', MockDice.value < wizard.spell_dc)
+        MockDice.set_roll(cantrips.CantripDamage.activate, '1d8', MockDice.value == 1)
+        wizard.cast(cantrips.acid_splash, 0, targets=[kobold1, kobold2])
+        self.assertEqual(kobold1.hp, 6)
+        self.assertEqual(kobold2.hp, 6-(1 + wizard.spellcasting))
+
+        # The wizard casts acid splash at the kobolds again, and both kobolds
+        # fail their dexterity saves, thus receiving acid damage. Note that this
+        # time the wizard targets the kobolds based on position.
+        MockDice.set_roll(kobold1.saving_throw, '1d20', MockDice.value < wizard.spell_dc)
+        MockDice.set_roll(kobold2.saving_throw, '1d20', MockDice.value < wizard.spell_dc)
+        MockDice.set_roll(cantrips.CantripDamage.activate, '1d8', MockDice.value == 1)
+        wizard.cast(cantrips.acid_splash, 0, targets=[kobold1.pos])
+        self.assertEqual(kobold1.hp, 6-(1 + wizard.spellcasting))
+        self.assertEqual(kobold2.hp, 6-(2*(1 + wizard.spellcasting)))
+
+        # One of the kobolds moves 10 feet away from the other kobold. The
+        # wizard tries to target both kobolds with acid splash resulting in a
+        # rules error.
+        kobold1.move((0,48))
+        self.assertRaises(
+            RulesError, wizard.cast, cantrips.acid_splash, 0, targets=[kobold1,kobold2]
+        )
+
+    @MockDice.patch('combatsim.creature', 'combatsim.cantrips')
+    def test_acid_splash_against_three_enemies(self):
+        # TODO test that three enemies can't be targeted
+        # TODO test targeting a location instead of a list of creatures
